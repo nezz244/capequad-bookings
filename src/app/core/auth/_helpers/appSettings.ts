6f9f5ef0501aa226093ca10a0281d0b587b3c99a
nezz <tnesara55@gmail.com>
2026-01-03 07:45:36 +0200

    updated endpoint url

--- src/app/core/auth/_helpers/appSettings.ts
+++ src/app/core/auth/_helpers/appSettings.ts
@@ -1,9 +1,9 @@
 export class AppSettings
 {
 
-  public static API_ENDPOINT = 'https://cape-quad-new112.wn.r.appspot.com';
+  public static API_ENDPOINT = 'http://capequad-backend-production.up.railway.app';
   // 'https://cape-quad-new112.wn.r.appspot.com';
-  
+
   public static API_ENDPOINT3 = 'https://api.chatsooner.com/v1';
   public static API_ENDPOINT4 = 'https://api.broadcast.chatinc.com/v1';
   // public static API_ENDPOINT = 'https://2f65-105-247-245-146.ngrok.io';
